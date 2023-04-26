214a70a0486166243bcf6ba7f34f0351c9fabac8
YuriiRodenko <jury.rodenko@gmail.com>
2023-04-26 11:37:10 +0300

    add task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryContainerEl.addEventListener("click", onClickGalleryItems);
+
+function onClickGalleryItems (e) {
+  e.preventDefault();
+
+  if (e.target.classList.value !== "gallery__image") {
+    return;
+  };
+
+    const source = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+
+
+
